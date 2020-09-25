8f5b574e4e90a6cfe52617a9140175318c747472
東岸 芳浩 <tohgishiyoshihiro@yoshihiroTOHGISHImacbookpro.local>
2020-09-25 12:26:41 +0900

    レイアウトの自動取得

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,16 @@
-console.log('scripts');
+//レイアウトターゲットの取得
+let layouts = {
+    header: document.getElementsByTagName('header')[0],
+    main: document.getElementsByTagName('main')[0],
+    aside: document.getElementsByTagName('aside')[0],
+    footer: document.getElementsByTagName('footer')[0],
+}
+//レイアウトの適用
+Object.keys(layouts).forEach(key => {
+    fetch('./layouts/' + key + '.html')
+        .then(response => response.text())
+        .then(data => {
+            layouts[key].innerHTML = data;
+        });
+});
+
